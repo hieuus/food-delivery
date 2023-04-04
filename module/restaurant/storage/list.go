38fed7c94cdedde4a9f888bfa3716f94706d73bd
Hieu Nguyen <nguyenvanhieu.gv2002@gmail.com>
2023-04-04 15:34:53 +0700

    list using seek method

--- module/restaurant/storage/list.go
+++ module/restaurant/storage/list.go
@@ -28,11 +28,30 @@ func (s *sqlStore) ListDataWithCondition(
 		return nil, common.ErrDB(err)
 	}
 
-	offset := (paging.Page - 1) * paging.Limit
+	//Using Seek Method
+	if v := paging.FakeCursor; v != "" {
+		uid, err := common.FromBase58(v)
+
+		if err != nil {
+			return nil, common.ErrDB(err)
+		}
+
+		db = db.Where("id < ?", uid.GetLocalID())
+	} else { //Offset method
+		offset := (paging.Page - 1) * paging.Limit
+		db = db.Offset(offset)
+	}
+
 	limit := paging.Limit
 
-	if err := db.Offset(offset).Limit(limit).Order("id desc").Find(&result).Error; err != nil {
+	if err := db.Limit(limit).Order("id desc").Find(&result).Error; err != nil {
 		return nil, common.ErrDB(err)
+	}
+
+	if len(result) > 0 {
+		last := result[len(result)-1]
+		last.Mask(false)
+		paging.NextCursor = last.FakeId.String()
 	}
 
 	return result, nil
